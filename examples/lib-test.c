0c272f54af44753dd1533d16988ccba1453a989d
Talonas <talonas@gmail.com>
2017-04-24 14:07:28 +0300

    Refactor example source

--- examples/lib-test.c
+++ examples/lib-test.c
@@ -4,31 +4,59 @@
 #include "lib.h"
 
 
+/**
+ * Calls "lib_get_number()" and checks returned value.
+ * It can be called using "CAT_FUNC_RUN(lib_get_number);"
+ */
+CAT_FUNC(lib_get_number)
+{
+	int ret = lib_get_number();
+	CAT_ASSERT(ret == 11);
+}
+
+/**
+ * Compares int values "a" and "b". Test will pass when they are equal.
+ * Otherwise, the test will fail.
+ * Can be called using "CAT_FUNC_RUN(compare_numbers, 1, 1);"
+ */
+CAT_FUNC(compare_numbers, int a, int b)
+{
+	CAT_ASSERT(a == b);
+}
+
+
 CAT_CASE(sample1)
 {
-	CAT_ASSERT_FINAL(1);
+	CAT_ASSERT(1);
 }
 
 CAT_CASE(sample2)
 {
-	CAT_ASSERT(10 > 1);
-	CAT_ASSERT_FINAL(1);
+	CAT_ASSERT(1 == 1);
+}
+
+CAT_CASE(sample3)
+{
+	CAT_FUNC_RUN(lib_get_number);
+}
+
+CAT_CASE(sample4)
+{
+	CAT_FUNC_RUN(compare_numbers, 9, 9);
 }
 
 CAT_CASE(lib_get_number_should_return_11)
 {
-	int ret;
-	ret = lib_get_number();
-	CAT_ASSERT_FINAL(ret == 11);
+	int ret = lib_get_number();
+	CAT_ASSERT(ret == 11);
 }
 
 CAT_CASE(lib_get_number_should_be_in_range)
 {
-	int ret;
-	ret = lib_get_number();
+	int ret = lib_get_number();
 	CAT_ASSERT(ret < 12);
 	CAT_ASSERT(ret > 10);
-	CAT_ASSERT_FINAL(ret == 11);
+	CAT_ASSERT(ret == 11);
 }
 
 CAT_CASE(lib_set_str_should_return_TEST)
@@ -39,7 +67,7 @@ CAT_CASE(lib_set_str_should_return_TEST)
 	ret = lib_set_str(str, sizeof(str));
 	CAT_ASSERT(ret == 0);
 	ret = strcmp(str, "TEST");
-	CAT_ASSERT_FINAL(ret == 0);
+	CAT_ASSERT(ret == 0);
 }
 
 CAT_CASE(lib_set_str_should_not_be_FEST)
@@ -50,5 +78,5 @@ CAT_CASE(lib_set_str_should_not_be_FEST)
 	ret = lib_set_str(str, sizeof(str));
 	CAT_ASSERT(ret == 0);
 	ret = strcmp(str, "FEST");
-	CAT_ASSERT_FINAL(ret != 0);
+	CAT_ASSERT(ret != 0);
 }
